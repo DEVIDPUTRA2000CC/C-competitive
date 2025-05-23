41d459e7fef96896defccdd9c93eebb886687b5f
DEVIDPUTRA2000CC <pranaykapadia289@gmail.com>
2025-05-24 00:02:28 +0530

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+// program 1
 int main(){
 	 int l, b;
 	cin>> l>> b;
 	cout <<l * 1LL * b<<endl; // here we use LL because to increase the range of the output  upto 10^18 to 10^18
-	
+} 
+// program 2
+int main(){
+	int t; // for test cases 
+	cin>>t;
+	while(t--){ 
+	int n;
+	cin>>n;
+	for(int i=1; i<=n; i++)	{
+		for(int j=1; j<=i; j++){
+			cout<<"*";
+		}
+	cout<< endl; 
+	}  
+
+
+	}
 
 
 
@@ -12,3 +29,11 @@ int main(){
 	 
 
 }
+
+
+
+
+
+	 
+
+
