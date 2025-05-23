ae0b2823f908dbd0009701ff85e641054ad0e0aa
DEVIDPUTRA2000CC <pranaykapadia289@gmail.com>
2025-05-23 18:39:37 +0530

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(){
 	 int l, b;
 	cin>> l>> b;
-	cout <<l * 1LL * b<<endl;
+	cout <<l * 1LL * b<<endl; // here we use LL because to increase the range of the output  upto 10^18 to 10^18
+	
 
 
 
