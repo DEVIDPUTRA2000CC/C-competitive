ea75e8bb76ba7a86061ebfa01d9ed1e68e801e09
DEVIDPUTRA2000CC <pranaykapadia289@gmail.com>
2025-06-05 23:32:13 +0530

    arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -74,6 +74,32 @@ int main() {
 	cout<<solve(v, n);
 }
 
+//largest element 
+int main(){
+	int mx=INT_MIN;
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+		
+	}
+
+	
+	for(int i=0; i<n; i++){
+		if(a[i]>mx){
+			mx = a[i]; 
+				}
+				
+        }
+        cout<<mx<<endl;
+        
+	}
+	return 0;
+	}
 
 
 	
