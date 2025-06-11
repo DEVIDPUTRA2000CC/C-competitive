1f319a4b93d19511102864deac3a71ad9fdf7f34
DEVIDPUTRA2000CC <pranaykapadia289@gmail.com>
2025-06-11 16:55:21 +0530

    arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -131,6 +131,25 @@ int main(){
 	}
 	
      }
+
+// Two sum probelm 
+class Solution {
+    int i, j;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mpp;
+	
+		for(int i =0; i<nums.size();i++){
+			int a = nums[i];
+			int more = target- a;
+			if(mpp.find(more)!=mpp.end()){
+				return {mpp[more], i};
+            }
+            mpp[a]=i;
+
+       return {};
+    }
+};
    
 
 
